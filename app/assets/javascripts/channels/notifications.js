89cc9c2a41ccec3cd16199fd6d9be36dcbe7262c
Manuel Sousa <mlrcbsousa@gmail.com>
2019-08-10 16:48:56 +0200

    notifications job

--- app/assets/javascripts/channels/notifications.js
+++ app/assets/javascripts/channels/notifications.js
@@ -1,14 +1,16 @@
-App.board = App.cable.subscriptions.create("NotificationsChannel", {
-  connected: function() {
-    // Called when the subscription is ready for use on the server
+if($('meta[name="current-user"]')[0].length > 0) {
+  App.board = App.cable.subscriptions.create("NotificationsChannel", {
+    connected: function() {
+      // Called when the subscription is ready for use on the server
 
-  },
-  disconnected: function() {
-    // Called when the subscription has been terminated by the server
+    },
+    disconnected: function() {
+      // Called when the subscription has been terminated by the server
 
-  },
-  received: function(data) {
-    console.log(data);
-    $('#notifications').prepend(data.html);
-  }
-})
+    },
+    received: function(data) {
+      console.log(data);
+      $('#notifications').prepend(data.html);
+    }
+  })
+}
---
+++ app/jobs/notification_relay_job.rb
@@ -0,0 +1,16 @@
+class NotificationRelayJob < ApplicationJob
+  queue_as :default
+
+  def perform(notification)
+    # Do something later
+    html = ApplicationController.render(
+      partial: "notifications/#{notification.notifiable_type.underscore.pluralize}/#{notification.action}",
+      locals: { notification: notification },
+      formats: [:html]
+    )
+    ActionCable.server.broadcast(
+      "notifications:#{notification.recipient_id}",
+      html: html
+    )
+  end
+end
